5c33242774453fef344fc90efe7557d663183235
mmr.K <pollvaulter.m@gmail.com>
2017-09-11 14:10:57 +0900

    add loop

--- ex4/loop_1.scala
+++ ex4/loop_1.scala
@@ -1,5 +1,16 @@
 object loop_1 {
   def main(args: Array[String]){
-      //ファイルだけ作って終了
+    println("while loop")
+    var i = 0
+    while(i<5){
+      println(i)
+      i+=1
+    }
+    println("do-while loop")
+    var j = 10
+    do{
+      println(j)
+    }while(i<5)
+
   }
 }
